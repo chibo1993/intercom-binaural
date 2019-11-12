623075a0836a8f9ccf8f88dc7a203eb6d23be5f5
chibo1993 <30491359+chibo1993@users.noreply.github.com>
2019-11-12 09:00:35 +0100

    Add files via upload

---
+++ intercom_binaural.py
@@ -0,0 +1,35 @@
+# Adding a buffer.
+
+import sounddevice as sd
+import numpy as np
+import struct
+from intercom import Intercom
+from intercom_buffer import Intercom_buffer
+from intercom_bitplanes import Intercom_bitplanes
+
+if __debug__:
+    import sys
+
+class Intercom_binaural(Intercom_bitplanes):
+
+    def init(self, args):
+        Intercom_bitplanes.init(self, args)
+        if self.number_of_channels == 2:
+            self.record_send_and_play = self.record_send_and_play_stereo
+
+    def record_send_and_play_stereo(self, indata, outdata, frames, time, status):
+        indata[:, 0] -= indata[:, 1]
+        self.send(indata)
+        self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
+        chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
+        chunk[:, 0] +=chunk[:, 1]
+        self._buffer[self.played_chunk_number % self.cells_in_buffer] = self.generate_zero_chunk()
+        self.played_chunk_number = (self.played_chunk_number + 1) % self.cells_in_buffer
+        outdata[:] = chunk
+
+if __name__ == "__main__":
+    intercom = Intercom_binaural()
+    parser = intercom.add_args()
+    args = parser.parse_args()
+    intercom.init(args)
+    intercom.run()
